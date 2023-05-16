42df2dd25290c96767aabb38af4726fef576d57b
Fubge <ramershoven.marvin@gmail.com>
2023-05-16 16:14:26 +0200

    Files updated

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// Get Elements from DOM
+const btn = document.getElementById('btn');
+const colorSpan = document.querySelector('.color');
+const bodyElement = document.body;
+
+// Add event listener for btn 
+btn.addEventListener('click', function() {
+    let randomColor = getRandomNumber();
+    bodyElement.style.backgroundColor = colors[randomColor];
+    colorSpan.innerHTML = colors[randomColor];
+});
+
+// Generate random number between 0 and max of array
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * colors.length);
+    return randomNumber;
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,27 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// Get Elements from DOM 
+const btn = document.getElementById('btn');
+const colorText = document.querySelector('.color');
+const bodyElement = document.body
+
+// Event Listener for Btn 
+btn.addEventListener('click', function() {
+    bodyElement.style.backgroundColor = createHexColor();
+    colorText.innerHTML = createHexColor(); 
+})
+// Create Hex Color based on random index
+function createHexColor() {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++)     
+        hexColor += hex[getrandomIndex()];
+    return hexColor;
+}
+// Create random Index for array
+function getrandomIndex() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+
+
+
